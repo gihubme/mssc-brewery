03b2b40cadfb6026266f99c9f6ba528fb38106fb
nikak <nikakar@gmail.com>
2022-05-11 11:13:04 +0200

    using 'val' and '@RequiredArgsConstructor' from lombok, changing spring-boot to 2.1.5.RELEASE

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,6 +2,8 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,15 +17,12 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-04-20.
  */
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
     private final BeerService beerService;
-
-    public BeerController(BeerService beerService) {
-        this.beerService = beerService;
-    }
 
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
@@ -34,9 +33,9 @@ public class BeerController {
     @PostMapping
     public ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto) {
 
-        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+        val savedDto = beerService.saveNewBeer(beerDto);
 
-        HttpHeaders headers = new HttpHeaders();
+        val headers = new HttpHeaders();
         //todo add hostname to url
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
